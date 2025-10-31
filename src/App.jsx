abac8a418dfd499c050cc85feed0212424727c2b
unknown <pranjalijamale@gmail.com>
2025-10-31 12:08:26 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='home'element={<Home/>}/>
           <Route path='about' element={<About/>}/>
           <Route path='login' element={<Login/>}/>
           <Route path='registerenquiry' element={<RegisterEnquiry/>}/>
